3cafb2969ddf43dee0ba59a50150ad91eeff839e
Lukas <papikdev@gmail.com>
2018-06-30 14:40:01 +0200

    heroku 🎉

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan's);
+const logger = require('morgan');
 
 const app = express();
 const http = require('http').Server(app);
